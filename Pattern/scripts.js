9eef91d9dabeb8cb3d5d0c9f0a2eeeea6bbf9ab2
ssux <ssux@pratt.edu>
2016-03-10 04:32:38 +0000

    wtf

--- Pattern/scripts.js
+++ Pattern/scripts.js
@@ -4,9 +4,21 @@ for (var i = 0; i < 25; i++) {
     $("body").append("<div class='sheep'></div>");
 };
 
-var randomNum = Math.floor(Math.random()*200) + "px";
+var bgArray = ["red", "blue", "black", "green", "white", "silver",
+    "coral", "darkred", "orange", "gold", "ivory", "lawngreen", "lime",
+    "mediumorchid", "midnightblue", "yellow", "steelblue"]
 
-$("#sheep").mouseenter(function(){
-    $(this).css("background-color", "blue" )
-    $(this).css('transform', 'translate(-' + randomNum + 'px)');
+
+$(".sheep").mouseenter(function(){
+    var randomNum = Math.floor(Math.random()*500) + "px";
+    var bgColor = bgArray[Math.floor(Math.random()*bgArray.length)];
+    $(this).css("background-color", bgColor)
+    $(this).css('transform', 'translateY(-' + randomNum + 'px)');
+});
+
+$(".sheep").mouseleave(function(){
+    var randomNum = Math.floor(Math.random()*500) + "px";
+    var bgColor2 = bgArray[Math.floor(Math.random()*bgArray.length)];
+    $(this).css("background-color", bgColor2)
+    $(this).css('transform', 'translateY(-' + randomNum + 'px)');
 });

7729053f9ffd46046e06884d3210cbd84b776abb
ssux <ssux@pratt.edu>
2016-04-07 13:47:21 +0000

    finished css

---
+++ catalogue/js/individualPage.js
@@ -0,0 +1,13 @@
+directions = []
+
+var clicked = false
+
+$("toggle").click(function() {
+    if (clicked == false) {
+        clicked =true;
+        $(".image").slideUp(.25, )
+    } 
+    else
+        clicked = false; 
+       
+)}
